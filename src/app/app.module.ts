27ef188836374290b634fcdbab0a67cbf6c3977d
McKenna Schaller <mckenna@clickswebdesign.com>
2019-09-27 14:20:41 -0500

    form add and remove functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  title = 'Projects';
+  pageTitle = 'Projects';
+  projects = [
+    { name: 'Animal Shelter Website',
+      image: 'http://placekitten.com/g/200/200',
+      description: 'Chase mice chase imaginary bugs, so stand in doorway, unwilling to chase dog.'
+    },
+    { name: 'Cats are Cute Graphic',
+      image: 'http://placekitten.com/g/400/400',
+      description: 'Chase cats run imaginary bugs, so stand in doorway, unwilling to chase dog.'
+    }
+  ];
+
+  name = '';
+  image = '';
+  description = '';
+
+  addProject(value) {
+    // Add to array
+    this.projects.push({ name: value.name, image: value.image, description: value.description});
+    // Clear inputs
+    this.name = '';
+    this.image = '';
+    this.description = '';
+  }
+
+  removeProject(project) {
+    this.projects.splice(project, 1);
+  }
 
   constructor() { }
 
